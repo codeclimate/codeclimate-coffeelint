8d13a7cefd0b9936018c30be2abcfd19f825588f
Blake Williams <blake@blakewilliams.me>
2015-11-19 11:43:48 -0500

    Rubocop happiness

--- spec/cc/engine/analyzable_files_spec.rb
+++ spec/cc/engine/analyzable_files_spec.rb
@@ -14,61 +14,61 @@ module CC::Engine
       FileUtils.rm_rf(tmp_dir)
     end
 
-    context "when config has include_paths" do
-      context "when given explicit paths" do
-        it "returns files matching only those paths" do
-          make_file("exclude.coffee")
-          make_file("foo/bar.coffee")
-          make_file("bar/foo.coffee")
+    context 'when config has include_paths' do
+      context 'when given explicit paths' do
+        it 'returns files matching only those paths' do
+          make_file('exclude.coffee')
+          make_file('foo/bar.coffee')
+          make_file('bar/foo.coffee')
 
           analyzable_files = AnalyzableFiles.new({
-            "include_paths" => ["foo/", "bar/"]
+            'include_paths' => ['foo/', 'bar/']
           })
           expect(analyzable_files.all.sort).to eq([
-            "bar/foo.coffee",
-            "foo/bar.coffee",
+            'bar/foo.coffee',
+            'foo/bar.coffee'
           ])
         end
       end
 
-      context "when given ./" do
-        it "returns all files in the current directory" do
-          make_file("root.coffee")
-          make_file("foo/bar.coffee")
-          make_file("foo/baz/wat.coffee")
+      context 'when given ./' do
+        it 'returns all files in the current directory' do
+          make_file('root.coffee')
+          make_file('foo/bar.coffee')
+          make_file('foo/baz/wat.coffee')
 
           analyzable_files = AnalyzableFiles.new({
-            "include_paths" => ["./"]
+            'include_paths' => ['./']
           })
           expect(analyzable_files.all.sort).to eq([
-            "foo/bar.coffee",
-            "foo/baz/wat.coffee",
-            "root.coffee",
+            'foo/bar.coffee',
+            'foo/baz/wat.coffee',
+            'root.coffee'
           ])
         end
       end
     end
 
-    context "when config has exclude_paths" do
-      it "excludes files that match exclude_paths" do
-        make_file("root.coffee")
-        make_file("foo/bar.coffee")
-        make_file("bar/foo.coffee")
+    context 'when config has exclude_paths' do
+      it 'excludes files that match exclude_paths' do
+        make_file('root.coffee')
+        make_file('foo/bar.coffee')
+        make_file('bar/foo.coffee')
 
         analyzable_files = AnalyzableFiles.new({
-          "exclude_paths" => ["bar/"]
+          'exclude_paths' => ['bar/']
         })
         expect(analyzable_files.all.sort).to eq([
-          "foo/bar.coffee",
-          "root.coffee"
+          'foo/bar.coffee',
+          'root.coffee'
         ])
       end
     end
 
     def make_file(name)
       FileUtils.mkdir_p(File.dirname(name))
-      File.open(name, "w") do |f|
-        f.write('console.log "hello!"')
+      File.open(name, 'w') do |f|
+        f.write("console.log 'hello!'")
       end
     end
   end
