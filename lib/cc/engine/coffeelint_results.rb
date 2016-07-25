61df8112939dde82d006b370f194a322e85cddb2
Chris Mendis <chris.mendis@gmail.com>
2016-07-25 11:21:01 -0400

    Use local npm-installed packages to power CoffeeScript linting

--- lib/cc/engine/coffeelint_results.rb
+++ lib/cc/engine/coffeelint_results.rb
@@ -13,7 +13,7 @@ module CC
         return [] if @files.empty?
 
         escaped_files = Shellwords.join(@files)
-        cmd = "coffeelint"
+        cmd = "/usr/src/app/node_modules/.bin/coffeelint"
         cmd << " -f #{@config}" if @config
         cmd << " -q --reporter raw #{escaped_files}"
         Dir.chdir(@directory) do
